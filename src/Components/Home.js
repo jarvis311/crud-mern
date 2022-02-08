05e7c0cf29e0e62cdd19f1d8e5edb3643a654fd8
jarvis <jignesh@gmail.com>
2022-02-08 09:36:28 +0530

    second commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,6 +6,7 @@ const Home = () => {
 
     <>
         <h1>This is Home page</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae ullam porro sint corrupti adipisci in aperiam, recusandae eaque, perferendis numquam voluptatum iure atque necessitatibus delectus!</p>
     
     </>
   )
